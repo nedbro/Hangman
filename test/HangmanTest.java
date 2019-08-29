20b37036324d42ae20a2d9cacb493610f50a5af3
Kocsis Robert <robertzkocsis@gmail.com>
2019-08-29 17:59:26 +0200

    Add test for the first and last word from the file

--- test/HangmanTest.java
+++ test/HangmanTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 public class HangmanTest {
     Hangman hangman = new Hangman();
     String wordToBeGuessed = "";
@@ -26,6 +28,30 @@ public class HangmanTest {
             if (returnedWord.equals(newReturnedWord)) {
                 Assertions.fail("The game gets the same word every time");
             }
+        }
+    }
+
+    @Test
+    public void testFirstAndLastWordFromTheFile() {
+        String expectedFirstWord = "the";
+        String expectedLastWord = "poison";
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            scanner = Hangman.createAnURLScanner();
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+
+        String readFirstWord = Hangman.getWordFromLocation(scanner,0);
+        String readLastWord = Hangman.getWordFromLocation(scanner,9998);
+
+        if(!expectedFirstWord.equals(readFirstWord)){
+            Assertions.fail("The expected and read first words don't match");
+        }
+
+        if(!expectedLastWord.equals(readLastWord)){
+            Assertions.fail("The expected and read first words don't match");
         }
     }
 }
